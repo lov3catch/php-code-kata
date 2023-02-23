aa8173bb377b02f5568677a3653795270170def9
ihor.kpl <lov3catch@gmail.com>
2023-02-23 10:46:16 +0200

    check doctrine collections

---
+++ collections.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+$collection = new ArrayCollection([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+$callbackFactory = function (int $gte) {
+    return function (int $insert)  use ($gte) {
+        return $insert > $gte;
+    };
+};
+
+$take2 = $callbackFactory(6);
+
+$inc1 = function ($element) {
+    return $element + 1;
+};
+
+$sum = function ($element, int $sum = 0) {
+    return $sum + $element;
+};
+
+$filteredCollection = $collection
+    ->filter($take2)
+    ->map($inc1)
+    ->reduce($sum);
+
+
+echo 'SUM: ' . $filteredCollection . PHP_EOL;
